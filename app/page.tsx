febb160359744c2b6129972dd303103400532b44
Matías Trunzo <matiasdanieltrunzo@gmail.com>
2023-07-02 18:26:10 -0300

    Cars api integration

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import Hero from '@/components/Hero'
 import Image from 'next/image'
 import SearchBar from '@/components/SearchBar'
 import CustomFilter from '@/components/CustomFilter'
+import CarCard from '@/components/CarCard'
+import { fetchCars } from '@/utils'
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars()
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -20,6 +26,21 @@ export default function Home() {
             <CustomFilter title="fuel" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__eror-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   )
