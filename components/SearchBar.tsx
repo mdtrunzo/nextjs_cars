0269c3179aaac21fe89bfafa7e430982d0192601
Matías Trunzo <matiasdanieltrunzo@gmail.com>
2023-07-02 01:29:32 -0300

    search components

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+'use client'
+import { useState } from 'react'
+import SearchManufacturer from '@/constants/SearchManufacturer'
 
 const SearchBar = () => {
-  return <div>SearchBar</div>
+  const [manufacturer, setManufacturer] = useState<string>('')
+
+  const handleSearch = () => {}
+
+  return (
+    <form className="searchBar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  )
 }
 
 export default SearchBar
--- types/index.ts
+++ types/index.ts
@@ -10,3 +10,8 @@ export interface CustomButtonProps {
 export interface CustomFilterProps {
   title: string
 }
+
+export interface SearchManufacturerProps {
+  manufacturer: string
+  setManufacturer: (manufacturer: string) => void
+}
