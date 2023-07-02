74192db081efbe7b87d41fee1e44eba940b5194e
Matías Trunzo <matiasdanieltrunzo@gmail.com>
2023-07-02 00:36:43 -0300

    first commit

---
+++ components/CustomButton.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { CustomButtonProps } from '@/types'
+import Image from 'next/image'
+
+const CustomButton = ({
+  title,
+  btnType,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  )
+}
+
+export default CustomButton
